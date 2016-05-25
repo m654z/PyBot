6ecb7a30e63793260de17a60a222490efabd38b6
William Muhu <wmuhucraft@gmail.com>
2016-05-25 17:47:23 +0300

    First!
    Added the main file and some docs.

---
+++ docs.rst
@@ -0,0 +1,51 @@
+PyBot Documentation
+********************
+
+PyBot is a chatbot that can be built on to create an AI. PyBot can learn
+from the user, so it demonstrates machine learning.
+
+Creating your own PyBot
+=======================
+
+Let's get started by creating a simple PyBot.
+All you need is the pyBot.py file and a copy of Python 3.
+
+To create a new bot, you'll want to do the following:
+
+    import pyBot
+	
+	myBot = pyBot.Bot() # Creates a bot named myBot.
+	while 1:
+		myBot.evaluate(input("Input > ")) # Evaluates the input and decides what to output.
+		
+It's as easy as that. You now have your very own chatbot!
+
+PyBot commands
+==============
+
+While chatting, you can use some commands to save or load the session, exit, and debug.
+
+/SAVE
+-----
+
+Save saves the current session, and stores it by default in a file called botSave.pkl.
+
+/LOAD
+-----
+
+Load loads a saved session from (by default) botSave.pkl
+
+/EXIT
+-----
+
+Exit exits the session.
+
+/DATA
+-----
+
+Data displays the bot's current database, as a Python dictionary. This command is useful for debugging.
+
+/HIST
+-----
+
+Hist displays the current session's history.
---
+++ pyBot.py
@@ -0,0 +1,54 @@
+import pickle
+import random
+
+class Bot:
+    def __init__(self, lastOutput, database, saveFile):
+        self.lastOutput = lastOutput
+        self.database = database
+        self.saveFile = saveFile
+        self.session = session
+
+    def save(self):
+        f = open(self.saveFile, 'wb')
+        pickle.dump(self.database, f)
+        f.close()
+
+    def load(self):
+        f = open(self.saveFile, 'wb')
+        self.database = pickle.load(f)
+        f.close()
+
+    def say(self, text):
+        print("BOT> " + text)
+        self.current = text
+        self.session.append("BOT> " + text + "\n")
+
+    def evaluate(self, text):
+        if text in self.database:
+            self.say(random.choice(self.database[text]))
+
+        if text == "/QUIT":
+            print("If you quit, you will lose your current database! Are you sure?")
+            userInput = input("y/n> ")
+            if userInput.lower().startswith("n"):
+                exit
+            else:
+                bot.say("...")
+
+        if text == "/SAVE":
+            self.save()
+            self.say("...")
+
+        if text == "/LOAD":
+            self.load()
+            self.say("Hi!")
+
+        if text == "/DATA":
+            print(self.database)
+            self.say("...")
+
+        if text == "/HIST":
+            print(self.session)
+            self.say("...")
+
+        self.session.append("YOU> " + text + "\n")
